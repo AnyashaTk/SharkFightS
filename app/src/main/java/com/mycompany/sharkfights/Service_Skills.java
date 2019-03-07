7bfc0bd7e02951dd201ccbc28a551b9411db3d7f
AnyashaTk <blydnait62@gmail.com>
2019-03-07 14:49:27 +0300

    first commit

--- app/src/main/java/com/mycompany/sharkfights/Service_Skills.java
+++ app/src/main/java/com/mycompany/sharkfights/Service_Skills.java
@@ -2,10 +2,40 @@ package com.mycompany.sharkfights;
 
 import android.app.Service;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.IBinder;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
+
+import android.view.Gravity;
+import android.graphics.PixelFormat;
+
 
 public class Service_Skills extends Service {
-    public Service_Skills() {
+
+    private WindowManager wmskills;
+    private LinearLayout llskills;
+
+
+    @Override
+    public void onCreate(){
+        super.onCreate();
+
+        wmskills = (WindowManager) getSystemService(WINDOW_SERVICE);
+        llskills= new LinearLayout(this);
+
+        LinearLayout.LayoutParams llskillsParameters = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+        llskills.setBackgroundColor(Color.argb(66,255,0,0));
+        llskills.setLayoutParams(llskillsParameters);
+
+        WindowManager.LayoutParams parameters = new WindowManager.LayoutParams(400,150, WindowManager.LayoutParams.TYPE_PHONE, WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE, PixelFormat.TRANSLUCENT);
+
+        parameters.x=0;
+        parameters.y=0;
+        parameters.gravity=Gravity.CENTER | Gravity.CENTER;
+
+
+        wmskills.addView(llskills, parameters);
     }
 
     @Override
